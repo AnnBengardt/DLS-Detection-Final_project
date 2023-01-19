d3d636a047a3fdc27fe9d8857c01ff5410a8996d
AnnBengardt <marunko.a@yandex.ru>
2023-01-19 08:39:42 +0300

    Initial commit with pretrained yolov5m
    Initial commit with pretrained yolov5m for first deploy on streamlit cloud

---
+++ app.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import torch
+from PIL import Image
+from io import *
+import glob
+from datetime import datetime
+import os
+import wget
+import time
+
+## CFG
+#cfg_model_path = "models/yourModel.pt"
+
+cfg_enable_url_download = False
+#if cfg_enable_url_download:
+    #url = "https://archive.org/download/yoloTrained/yoloTrained.pt"  # Configure this if you set cfg_enable_url_download to True
+    #cfg_model_path = f"models/{url.split('/')[-1:][0]}"  # config model path from url name
+
+
+## END OF CFG
+
+
+def pretrained_yolov5(device, src):
+    if src == 'Загрузить изображение':
+        image_file = st.file_uploader("Загрузить изображение:", type=['png', 'jpeg', 'jpg'])
+        col1, col2 = st.columns(2)
+        if image_file is not None:
+            img = Image.open(image_file)
+            with col1:
+                st.image(img, caption='Загруженное', use_column_width='always')
+            ts = datetime.timestamp(datetime.now())
+            imgpath = os.path.join('data/uploads', str(ts) + image_file.name)
+            outputpath = os.path.join('data/outputs', os.path.basename(imgpath))
+            with open(imgpath, mode="wb") as f:
+                f.write(image_file.getbuffer())
+
+            # call Model prediction--
+            model = torch.hub.load("ultralytics/yolov5", "yolov5m", pretrained=True)
+            #model = torch.hub.load('ultralytics/yolov', 'custom', path='models/yoloTrained.pt', force_reload=True)
+            model.cuda() if device == 'cuda' else model.cpu()
+            pred = model(imgpath)
+            pred.render()  # render bbox in image
+            for im in pred.ims:
+                im_base64 = Image.fromarray(im)
+                im_base64.save(outputpath)
+
+            # --Display predicton
+
+            img_ = Image.open(outputpath)
+            with col2:
+                st.image(img_, caption='Результат', use_column_width='always')
+
+    elif src == 'Изображения из тестовой выборки':
+        # Image selector slider
+        imgpath = glob.glob('data/images/*')
+        imgsel = st.slider('Выбрать случайную картинку', min_value=1, max_value=len(imgpath), step=1)
+        image_file = imgpath[imgsel - 1]
+        submit = st.button("Начать детекцию")
+        col1, col2 = st.columns(2)
+        with col1:
+            img = Image.open(image_file)
+            st.image(img, caption='Выбранное изображение', use_column_width='always')
+        with col2:
+            if image_file is not None and submit:
+                # call Model prediction--
+                model = torch.hub.load("ultralytics/yolov5", "yolov5m", pretrained=True)
+                pred = model(image_file)
+                pred.render()  # render bbox in image
+                for im in pred.ims:
+                    im_base64 = Image.fromarray(im)
+                    im_base64.save(os.path.join('data/outputs', os.path.basename(image_file)))
+                    # --Display predicton
+                    img_ = Image.open(os.path.join('data/outputs', os.path.basename(image_file)))
+                    st.image(img_, caption='Результат')
+
+def main():
+
+    datasrc = st.sidebar.radio("Загрузить", ['Изображение из тестовой выборки', 'Своё любое изображение'])
+
+    option = st.sidebar.radio("Модель", ['Pretrained YOLOv5m', 'Обученная на кастомном датасете ...'])
+    if torch.cuda.is_available():
+        deviceoption = st.sidebar.radio("Устройство", ['CPU', 'Cuda'], disabled=False, index=1)
+    else:
+        deviceoption = st.sidebar.radio("Устройство", ['CPU', 'Cuda'], disabled=True, index=0)
+    # -- End of Sidebar
+
+    st.header('Проект для DLS (семестр Осень, 2022): Detection')
+    st.subheader("Автор: Марунько Анна")
+    st.sidebar.markdown("Здесь будет ссылка на гитхаб")
+    if option == "Pretrained YOLOv5m":
+        pretrained_yolov5(deviceoption, datasrc)
+    elif option == "Обученная на кастомном датасете ...":
+        pass
+
+
+if __name__ == '__main__':
+    main()
+
+
+# Downlaod Model from url.
+@st.cache
+def loadModel(url):
+    start_dl = time.time()
+    model_file = wget.download(url, out="models/")
+    finished_dl = time.time()
+    print(f"Model Downloaded, ETA:{finished_dl - start_dl}")
+
+
+if cfg_enable_url_download:
+    loadModel()
