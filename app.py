db29068b2c3dd68e0341cf45765ab3d9c8ffb864
AnnBengardt <marunko.a@yandex.ru>
2023-01-25 00:00:15 +0300

    New interface, new custom model, test images

--- app.py
+++ app.py
@@ -20,14 +20,8 @@ cfg_enable_url_download = False
 ## END OF CFG
 
 
-def pretrained_yolov5(device, src):
+def pretrained_yolov5(device="CPU"):
         image_file = st.file_uploader("Загрузить изображение:", type=['png', 'jpeg', 'jpg'])
-
-        imgpath = glob.glob('data/images/*')
-        imgsel = st.slider('Выбрать случайную картинку', min_value=1, max_value=len(imgpath), step=1)
-        selected_image_file = imgpath[imgsel - 1]
-        submit = st.button("Начать детекцию")
-
         col1, col2 = st.columns(2)
         if image_file is not None:
             img = Image.open(image_file)
@@ -41,7 +35,6 @@ def pretrained_yolov5(device, src):
 
             # call Model prediction--
             model = torch.hub.load("ultralytics/yolov5", "yolov5m", pretrained=True)
-            #model = torch.hub.load('ultralytics/yolov', 'custom', path='models/yoloTrained.pt', force_reload=True)
             model.cuda() if device == 'cuda' else model.cpu()
             pred = model(imgpath)
             pred.render()  # render bbox in image
@@ -56,36 +49,46 @@ def pretrained_yolov5(device, src):
                 st.image(img_, caption='Результат', use_column_width='always')
 
 
-        elif selected_image_file is not None:
-            image_file = selected_image_file
-            with col1:
-                img = Image.open(image_file)
-                st.image(img, caption='Выбранное изображение', use_column_width='always')
-            with col2:
-                if image_file is not None and submit:
-                    # call Model prediction--
-                    model = torch.hub.load("ultralytics/yolov5", "yolov5m", pretrained=True)
-                    pred = model(image_file)
-                    pred.render()  # render bbox in image
-                    for im in pred.ims:
-                        im_base64 = Image.fromarray(im)
-                        im_base64.save(os.path.join('data/outputs', os.path.basename(image_file)))
-                        # --Display predicton
-                        img_ = Image.open(os.path.join('data/outputs', os.path.basename(image_file)))
-                        st.image(img_, caption='Результат')
+def custom_yolov5s(device="CPU"):
+    st.header('Обученная YOLOv5s на кастомном датасете')
+    st.subheader("Датасет: виды перерабатываемого и неперерабатываемого мусора")
+    st.text("В перспективе такую модель можно использовать на мусороперерабатывающий заводе для быстрой сортировки или для дронов, собирающих мусор в природных зонах вроде лесов и океанов.")
+    st.image("metrcis_screenshot.png", caption="Лосс и метрики обученной модели")
+    imgpath = glob.glob('data/images/*')
+    imgsel = st.slider('Выбрать случайную картинку из тестовой выборки', min_value=1, max_value=len(imgpath), step=1)
+    image_file = imgpath[imgsel - 1]
+    submit = st.button("Начать детекцию")
+    col1, col2 = st.columns(2)
+    with col1:
+        img = Image.open(image_file)
+        st.image(img, caption='Выбранное изображение', use_column_width='always')
+    with col2:
+        if image_file is not None and submit:
+            # call Model prediction--
+            model = torch.hub.load("ultralytics/yolov5", "custom", path='data/models/yoloTrained.pt', force_reload=True)
+            pred = model(image_file)
+            pred.render()  # render bbox in image
+            for im in pred.ims:
+                im_base64 = Image.fromarray(im)
+                im_base64.save(os.path.join('data/outputs', os.path.basename(image_file)))
+                # --Display predicton
+                img_ = Image.open(os.path.join('data/outputs', os.path.basename(image_file)))
+                st.image(img_, caption='Результат')
+
+
 
 def main():
 
-    option = st.sidebar.radio("Модели", ['Pretrained YOLOv5m', 'Обученная на кастомном датасете YOLOv5s (изображения)',
-                                         'Обученная на кастомном датасете YOLOv5s (видео)'])
-
+    option = st.sidebar.radio("Модель", ['Pretrained YOLOv5m', 'Pretrained YOLOv5m - video','Custom dataset YOLOv5s'])
 
     st.header('Проект для DLS (семестр Осень, 2022): Detection')
     st.subheader("Автор: Марунько Анна")
     st.sidebar.markdown("https://github.com/AnnBengardt/DLS-Detection-Final_project")
     if option == "Pretrained YOLOv5m":
-        pretrained_yolov5("0")
-    elif option == "Обученная на кастомном датасете ...":
+        pretrained_yolov5()
+    elif option == "Custom dataset YOLOv5s":
+        custom_yolov5s()
+    elif option == "Pretrained YOLOv5m - video":
         pass
 
 
