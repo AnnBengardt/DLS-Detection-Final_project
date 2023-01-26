90681e67434f09fd5c28c973b71da845828576cb
AnnBengardt <marunko.a@yandex.ru>
2023-01-26 22:39:17 +0300

    Output path fix

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def video_custom_yolov5s():
         video_bytes = st_video.read()
         st.video(video_bytes)
         st.write("Загруженное видео")
-        run(weights='data/models/yoloTrained.pt', source=imgpath, device="cpu")
+        run(weights='data/models/yoloTrained.pt', source=imgpath, device="cpu", project='data/outputs')
         st_video2 = open(outputpath, 'rb')
         video_bytes2 = st_video2.read()
         st.video(video_bytes2)
